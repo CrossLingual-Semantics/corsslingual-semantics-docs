0456dd570c708e75a67a6ec3f247a81ac6546c9f
Kabir Manandhar Shrestha <manandharshr@rs-l-die94y.desktop.cloud.unimelb.edu.au>
2025-02-17 14:12:43 +1100

    Added Sphinx documentation

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,17 @@
+.. SWOW Enhance Documentation documentation master file, created by
+   sphinx-quickstart on Mon Feb 17 14:07:48 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SWOW_Enhanced's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   installation
+   usage
+   api_reference
+
---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SWOW Enhance Documentation'
+copyright = '2025, Kabir, Geordie, Lea, Chunhua, Simon'
+author = 'Kabir, Geordie, Lea, Chunhua, Simon'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
